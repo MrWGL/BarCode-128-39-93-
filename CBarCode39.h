908c15ef4cfed360d50c5b954ff3f58d4ea669d3
MrWGL <36329791+MrWGL@users.noreply.github.com>
2019-08-26 09:29:03 +0800

    Add files via upload

---
+++ CBarCode39.h
@@ -0,0 +1,82 @@
+﻿#pragma once
+/*
+功能:  BarCode39条码生成
+时间:  2019-5-27
+Editor:  Mr.Wu
+*/
+
+#ifndef CBARCODE39
+#define CBARCODE39
+
+#include "BarCodeItem/barcodelib_global.h"
+#include <string>
+#include <vector>
+#include <math.h>
+#include <stdlib.h>
+
+using namespace std;
+
+
+/**
+*\brief
+*编码元素,存储了编码值，及编码索引，
+*/
+struct element39
+{
+	string value;
+	string code39;
+	string encoding;
+
+	element39(){}
+	element39(string svalue,string scode39,string sencoding):
+		value(svalue),code39(scode39),encoding(sencoding){}
+};
+
+class BARCODELIB_EXPORT CBarCode39
+{
+public:
+	CBarCode39(string);
+	CBarCode39();
+	~CBarCode39();
+
+	/**
+	*\brief
+	*获取字符编码
+	*/
+	string GetEncode39();
+
+	/**
+	*\brief
+	*初始化BarCode39
+	*/
+	void InitCode();
+
+	/**
+	*\brief
+	*查找字符位置编码
+	*/
+	int FindCodePosition(string s);
+
+	string text()const;
+
+private:
+	string     m_RawData;                //原始内容
+
+	vector<element39*>*  m_Encoding;               //字符编码集
+
+	vector<string>   m_FormatData;
+
+private:
+	/**
+	*\brief
+	*字符转换，全部转换成大写
+	*/
+	void StringToUpper(string &str);
+};
+
+
+#endif // !CBARCODE39
+
+
+
+
---
+++ CBarCode93.h
@@ -0,0 +1,94 @@
+﻿#pragma once
+/*
+功能: BarCode93条码生成
+时间: 2019-5-28
+Editor: Mr.Wu
+*/
+
+
+#ifndef CBARCODE93
+#define CBARCODE93
+
+#include <vector>
+#include <string>
+#include <math.h>
+#include <stdlib.h>
+#include"BarCodeItem/barcodelib_global.h"
+
+using namespace std;
+
+struct element93
+{
+	string value;
+	string code;
+	string enconding;
+	element93(){}
+	element93(string svalue,string scode,string sencode):
+		value(svalue),code(scode),enconding(sencode){}
+};
+
+class BARCODELIB_EXPORT CBarCode93
+{
+public:
+	CBarCode93();
+	CBarCode93(string);
+	~CBarCode93();
+
+	/**
+	*\brief
+	*获取BarCode93编码
+	*/
+	string GetEncoding();
+	
+	/**
+	*\brief
+	*将文本转大写字母
+	*/
+	void StringToUpper(std::string &text);
+
+	string text()const;
+
+public:
+	/**
+	*\brief
+	*初始化编码
+	*/
+	void InitCode93();
+
+	/**
+	*\brief
+	*计算C检验码
+	*/
+	int CalculateCCheckDigit();
+
+	/**
+	*\brief
+	*计算K检验码
+	*/
+	int CalculateKCheckDigit();
+
+	/**
+	*\brief
+	*在初始化编码中,查找字符位置，返回字符位置
+	*/
+	int FindCodePosition(string s);
+
+	/**
+	*\brief
+	*插入检验码,返回一个新的字符串
+	*/
+	void InsertCheckDigitCode(string &currentstr,string s);
+
+private:
+	vector<element93*> *m_Code93;   //字符编码表
+
+	string m_RawData;       //原始数据
+
+	string m_Encode;        //加上检验码后的字符
+};
+
+
+#endif // !CBARCODE93
+
+
+
