bfd58e903e6e2eeed08d51a361bd884aa5d074d7
MrWGL <36329791+MrWGL@users.noreply.github.com>
2019-08-26 09:42:49 +0800

    Create define.h

---
+++ include/define.h
@@ -0,0 +1,63 @@
+#pragma once
+
+/*
+Code128 编码字符定义
+*/
+#define NUL "\0"
+#define SOH "\x01"
+#define STX "\x02"
+#define ETX "\x03"
+#define EOT "\x04"
+#define ENQ "\x05"
+#define ACK "\x06"
+#define BEL "\x07"
+#define BS  "\x08"
+#define TAB "\x09"
+#define LF  "\x0A"
+#define VT  "\x0B"
+#define FF  "\x0C"
+#define CR  "\x0D"
+#define SO  "\x0E"
+#define SI  "\x0F"
+#define DLE "\x10"
+#define DC1 "\x11"
+#define DC2 "\x12"
+#define DC3 "\x13"
+#define DC4 "\x14"
+#define NAK "\x15"
+#define SYN "\x16"
+#define ETB "\x17"
+#define CAN "\x18"
+#define EM  "\x19"
+#define SUB "\x1A"
+#define ESC "\x1B"
+#define FS  "\x1C"
+#define GS  "\x1D"
+#define RS  "\x1E"
+#define US  "\x1F"
+#define DEL "\x7F"
+
+#define FNC1 "FNC1"
+#define FNC2 "FNC2"
+#define FNC3 "FNC3"
+#define FNC4 "FNC4"
+
+#define SHIFT "SHIFT"
+#define START_A "AO"
+#define START_B "BO"
+#define START_C "CO"
+
+#define CODE_A "CODE_A"
+#define CODE_B "CODE_B"
+#define CODE_C "CODE_C"
+
+#define STOP "STOP"
+
+#define ON "\xFF"
+#define OFF "\x00"
+
+
+/*
+Code 39编码字符定义
+*/
+
